90235a3ed4865873889d04467dca9e9bf340ca95
alester77 <113203488+alester77@users.noreply.github.com>
2022-11-09 22:00:16 -0700

    added time to js

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,9 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
+
+$(function() {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -9,7 +11,19 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+
   // TODO: Add code to apply the past, present, or future class to each time
+  function colorForTime () {
+    var timeSpot = document.createElement("h4");
+    var today = dayjs();
+    var time = dayjs().format('hh:mm:ss');
+    $('#time').text(time);
+    $('#time').text(today);
+    console.log(time);
+  }
+  colorForTime();
+  
+  
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
@@ -21,3 +35,5 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+
